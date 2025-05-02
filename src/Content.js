781712ce6962db3785ec4e1e963222557599e637
AbubakarDev <abubakarjilani99@gmail.com>
2025-05-02 22:55:50 +0500

    Code clean and show toast list on content page

--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import ToastList from './components/ToastList';
+import LikedList from './components/LikedList';
+import { useToastManager } from './custom-hook/useToastManager';
 
 export default function Content() {
+  const { toasts, liked, likeToast, dismissToast } = useToastManager();
+
   return (
     <Box sx={{marginTop: 3}}>
       <Typography variant="h4">Liked Form Submissions</Typography>
-
-      <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
-        TODO: List of liked submissions here (delete this line)
-      </Typography>
+      <ToastList toasts={toasts} onLike={likeToast} onDismiss={dismissToast} />
+      <LikedList liked={liked} />
     </Box>
   );
 }
--- src/components/LikedList.js
+++ src/components/LikedList.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function LikedList({ liked }) {
   return (
     <div>
-      <h2>Liked Submissions</h2>
       <ul>
         {liked.map((item) => (
           <li key={item.id}>
